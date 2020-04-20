50bb70eef636b95c8296dbdea034614cad3bb336
Emin <eminaydin93@yahoo.com>
2020-04-20 14:38:06 +0200

    divs aligned

--- src/App.js
+++ src/App.js
@@ -10,23 +10,23 @@ class App extends React.Component {
   }
 colorGenerator() {
 document.querySelector("h2").className = "hidden";
-
+document.querySelector(".colored-divs").style.display= "flex";
     let letters = "0123456789ABCDEF"; 
     let color = '#'; 
     for (let i = 0; i < 6; i++) 
        color += letters[(Math.floor(Math.random() * 16))];
       
-       let divs = document.createElement("li");
-       divs.innerHTML =  `
-       <div class="color-divs"></div>`
+       let divs = document.createElement("div");
+       divs.className = "visible"
        divs.style.backgroundColor = color;
-       document.querySelector(".App").appendChild(divs)
+       document.querySelector(".colored-divs").appendChild(divs)
        
 }
   render() {
     return (
       <div className="App">
         <h2>There are no colors yet.</h2>
+        <div className="colored-divs"></div>
         <button onClick={this.colorGenerator}>Create a color</button>
       </div>
     );
